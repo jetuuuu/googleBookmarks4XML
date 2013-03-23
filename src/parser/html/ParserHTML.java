5a4f5dda27cfa264e5e113a74e16c502f50d017d
Nikita Galushko <galnikrom@gmail.com>
2013-03-23 19:58:32 +0400

    add regex for parsing ADD_DATE (version 0.2)

--- src/parser/html/ParserHTML.java
+++ src/parser/html/ParserHTML.java
@@ -1,5 +1,6 @@
 package parser.html;
 
+import javax.imageio.stream.FileImageInputStream;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,12 +18,15 @@ public class ParserHTML {
     private String dir;
     private ArrayList<String> listURL;
     private ArrayList<String> listName;
+    private ArrayList<String> listDate;
     private String patternURL = "http:\\S+\"[^\"]*\"";
     private String patternName = "<a(.*)>(.*)<\\Sa>";
+    private String patternDate = "[0-9]{10,70}";
 
     public ParserHTML(String path) {
         this.listURL = new ArrayList<String>();
         this.listName = new ArrayList<String>();
+        this.listDate = new ArrayList<String>();
         this.dir = path;
     }
 
@@ -61,14 +65,34 @@ public class ParserHTML {
                 Pattern p = Pattern.compile(this.patternName, Pattern.CASE_INSENSITIVE);
                 Matcher m = p.matcher(text);
 
-                if (m.find()) {
+                if (m.find())
                     this.listName.add(m.group(2));
-                }
             }
 
             reader.close();
         }
         catch(Exception error) {
+            error.printStackTrace();
+        }
+    }
+
+    private void parseDate() throws FileNotFoundException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(this.dir)));
+        String text;
+
+        try {
+            while((text = reader.readLine()) != null) {
+                Pattern p = Pattern.compile(this.patternDate, Pattern.CASE_INSENSITIVE);
+                Matcher m = p.matcher(text);
+
+                if (m.find())
+                    this.listDate.add(m.group());
+            }
+
+            reader.close();
+        }
+        catch (Exception error) {
             error.printStackTrace();
         }
     }
@@ -98,4 +122,16 @@ public class ParserHTML {
 
     }
 
+    public ArrayList<String> getDate() {
+
+        try {
+            this.parseDate();
+        }
+        catch (Exception error) {
+            error.printStackTrace();
+        }
+
+        return this.listDate;
+
+    }
 }
